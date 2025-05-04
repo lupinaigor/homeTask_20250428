cf36c0048359177ea2b120fabe6285e83e02b5c1
LupinaIgor <igorlupina222@gmail.com>
2025-05-04 09:04:46 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer, useRoute } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function UserList({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>User List Screen</Text>
+            <Button
+                title="Go to User Details"
+                onPress={() => navigation.navigate('UserDetails', { userName: 'John Doe' })}
+            />
+        </View>
+    );
+}
+
+function UserDetails() {
+    const route = useRoute();
+    const { userName } = route.params;
+
+    return (
+        <View style={styles.container}>
+            <Text>User Details Screen</Text>
+            <Text>Name: {userName}</Text>
+        </View>
+    );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="UserList" component={UserList} />
+                <Stack.Screen name="UserDetails" component={UserDetails} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
